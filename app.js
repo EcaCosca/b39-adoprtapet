03638fae7e875f1b77d3e083b94dbf701786a8fa
EcaCosca <enriquecoscarelli@gmail.com>
2023-06-16 13:54:35 +0200

    Done with step 4

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8000
 
 const pets = require('./petList')
 
@@ -8,7 +8,7 @@ const pets = require('./petList')
 app.get('/', (req, res) => {
   res.send(`<h1>Adopt a Pet!</h1>`)
 })
-
+// 4. Bind the server to a port with the app.listen() method, with a port of your choice (for this exercise we suggest 8000 but any number between ports 1024 and 65535 is valid). Start your server with node app.js (or npm run dev if you installed Nodemon) and open http://localhost:8000 on your browser. You should see the heading displayed on the page!
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port http://localhost:${port}`)
 })
