91461fac9c57cdf7185467978cc773c38f77152b
EcaCosca <enriquecoscarelli@gmail.com>
2023-06-16 13:51:17 +0200

    Step 3 done

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ const port = 3000
 
 const pets = require('./petList')
 
-console.log(pets)
-
+// 3. We can create our first route with app.get() on the `/` route. This endpoint should return an <h1> element with the text ‘Adopt a Pet!’. HTML can be returned as a Template literal.
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(`<h1>Adopt a Pet!</h1>`)
 })
 
 app.listen(port, () => {
