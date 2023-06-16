5a38d6e6a4663bb43b8be484e552e1dd10696e7f
EcaCosca <enriquecoscarelli@gmail.com>
2023-06-16 15:28:57 +0200

    test done and working

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 const express = require('express')
 const app = express()
+const pets = require('./petList')
 const port = 8000
 
-const pets = require('./petList')
 
-// 3. We can create our first route with app.get() on the `/` route. This endpoint should return an <h1> element with the text ‘Adopt a Pet!’. HTML can be returned as a Template literal.
-// 5. Let’s have our server output more elements to the page. Inside the res.send() method for your root route, add the following after the <h1> element:
-// An <p> element that reads ‘Browse through the links below to find your new furry friend:’.
-// An <ul> element with three <li> elements. The unordered list should contain three items: ‘Dogs’, ‘Cats’, and ‘Rabbits’.
 app.get('/', (req, res) => {
   res.send(`
   <h1>Adopt a Pet!</h1>
@@ -20,8 +16,47 @@ app.get('/', (req, res) => {
   `)
 })
 
+app.get('/animals/:pet_type', (req, res) => {
+    const petType = req.params.pet_type;
+  
+    if (petType === "dogs" || petType === "cats" || petType === "rabbits") {
+      const typeList = pets[petType];
+      
+      res.send(`
+        <h1>${petType.toUpperCase()}</h1>
+        <ul>
+            ${typeList.map((element, index) => `
+                <li>
+                    <a href="/animals/${petType}/${index}">
+                        ${element.name}
+                    </a>
+                </li>
+            `)}
+        </ul>`);
+    } else {
+      res.send("<h1>404</h1>");
+    }
+})
 
-// 4. Bind the server to a port with the app.listen() method, with a port of your choice (for this exercise we suggest 8000 but any number between ports 1024 and 65535 is valid). Start your server with node app.js (or npm run dev if you installed Nodemon) and open http://localhost:8000 on your browser. You should see the heading displayed on the page!
+app.get('/animals/:pet_type/:pet_id', (req, res) => {
+    const findPet = pets[req.params.pet_type][Number(req.params.pet_id)];
+    
+    if(findPet){
+        res.send(`
+        <h1>${findPet.name}</h1>
+        <img src="${findPet.url}" alt=${findPet.name}>
+        <p>${findPet.description}</p>
+        <ul>
+        <li>Breed: ${findPet.breed}</li>
+        <li>Age: ${findPet.age}</li>
+        </ul>
+        `)
+    }else{
+        res.send("<h1>404</h1>");
+    }
+    
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
 })
