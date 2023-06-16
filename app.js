d5313c42bbeb48e2cf46318a6297e071970faf6f
EcaCosca <enriquecoscarelli@gmail.com>
2023-06-16 15:36:39 +0200

    step 6 done

--- app.js
+++ app.js
@@ -4,10 +4,6 @@ const port = 8000
 
 const pets = require('./petList')
 
-// 3. We can create our first route with app.get() on the `/` route. This endpoint should return an <h1> element with the text ‘Adopt a Pet!’. HTML can be returned as a Template literal.
-// 5. Let’s have our server output more elements to the page. Inside the res.send() method for your root route, add the following after the <h1> element:
-// An <p> element that reads ‘Browse through the links below to find your new furry friend:’.
-// An <ul> element with three <li> elements. The unordered list should contain three items: ‘Dogs’, ‘Cats’, and ‘Rabbits’.
 app.get('/', (req, res) => {
   res.send(`
   <h1>Adopt a Pet!</h1>
@@ -20,8 +16,15 @@ app.get('/', (req, res) => {
   `)
 })
 
+// 6. We have our root route nicely underway! Next we need a route to show lists of each animal type: dogs, cats and rabbits. To do that, we’ll add a new app.get(), with /animals/:pet_type as our path, where :pet_type is a dynamic parameter.
+app.get('/animals/:pet_type', (req, res) => {
+    const petType = req.params.pet_type;
 
-// 4. Bind the server to a port with the app.listen() method, with a port of your choice (for this exercise we suggest 8000 but any number between ports 1024 and 65535 is valid). Start your server with node app.js (or npm run dev if you installed Nodemon) and open http://localhost:8000 on your browser. You should see the heading displayed on the page!
+  res.send(`
+  ${petType}
+  `)
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
 })
